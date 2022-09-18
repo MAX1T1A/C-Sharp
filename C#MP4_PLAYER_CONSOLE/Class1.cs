c37c1cab3abab293e70d0fddeba02ee793f50e17
MAX1T1A <113845928+MAX1T1A@users.noreply.github.com>
2022-09-18 19:43:11 +0300

    Add files via upload

---
+++ C#MP4_PLAYER_CONSOLE/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ASCTwo
+{
+    class Class1
+    {
+        static Bitmap GenerateQRCode()
+        {
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250,
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+
+            var qr = new ZXing.BarcodeWriter();
+            qr.Options = options;
+            qr.Format = ZXing.BarcodeFormat.QR_CODE;
+            var result = new Bitmap(qr.Write("Anton is a shit coder"));
+
+            Image bmp = new Bitmap(result);
+
+            bmp.Save(@"C:\Users\00ARM-IT\Desktop\Hacker\QR.png", ImageFormat.Png);
+
+            return result;
+        }
+
+        static void DecodeQRCode(Bitmap qrCode)
+        {
+            Bitmap bitmap = new Bitmap(qrCode);
+            BarcodeReader reader = new BarcodeReader { AutoRotate = true };
+            Result result = reader.Decode(bitmap);
+            string decoded = result.ToString().Trim();
+            Console.WriteLine(decoded);
+        }
+
+        static void Main(string[] args)
+        {
+            DecodeQRCode(GenerateQRCode());
+        }
+    }
+}
---
+++ C#MP4_PLAYER_CONSOLE/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace ASCTwo
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 2;
+        private const int MAX_WIDTH = 100;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.png"
+            };
+
+            string[] files = Directory.GetFiles("Путь к файлу с кадрами");
+
+            int count = 0;
+            while (true)
+            {
+                var bitmap = new Bitmap(files[count]);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                Console.SetCursorPosition(0, 0);
+
+                if (count < files.Length)
+                    count += 1;
+                else
+                    break;
+                
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            return bitmap;
+        } 
+
+    }
+}
