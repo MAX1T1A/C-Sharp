b2c52c4a4a2d3936a685501d6974960e7ad61e4c
MAX1T1A <113845928+MAX1T1A@users.noreply.github.com>
2022-09-18 19:20:22 +0300

    Add files via upload

---
+++ CodingHorizontal.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QRCodeGenerate
+{
+    class CodingHorizontal
+    {
+        public static void EndWay(object[][] qrCode, string words, int y1, int y2, int x1, int x2, int x3)
+        {
+            var list = new List<int>();
+
+            var oneList = new List<int> { 1, 2, 4, 8, 16, 32 };
+
+            var twoList = new List<int> { 3, 5, 6, 9, 10, 12, 17, 18, 20, 24, 33 };
+            var threeList = new List<int> { 7, 11, 13, 14, 19, 21, 22, 25, 26, 28 };
+            var fourList = new List<int> { 15, 23, 27, 29, 30 };
+
+
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                ["а"] = 1,//  1
+                ["б"] = 2,//  1
+                ["в"] = 3,//  2 
+                ["г"] = 4,//  1
+                ["д"] = 5,//  2
+                ["е"] = 6,//  2
+                ["ё"] = 7,//  3
+                ["ж"] = 8,//  1
+                ["з"] = 9,//  2
+                ["и"] = 10,// 2
+                ["й"] = 11,// 3
+                ["к"] = 12,// 2
+                ["л"] = 13,// 3
+                ["м"] = 14,// 3
+                ["н"] = 15,// 4
+                ["о"] = 16,// 1
+                ["п"] = 17,// 2
+                ["р"] = 18,// 2
+                ["с"] = 19,// 3
+                ["т"] = 20,// 2
+                ["у"] = 21,// 3
+                ["ф"] = 22,// 3
+                ["х"] = 23,// 4
+                ["ц"] = 24,// 2
+                ["ч"] = 25,// 3
+                ["ш"] = 26,// 3
+                ["щ"] = 27,// 4
+                ["ъ"] = 28,// 3
+                ["ь"] = 29,// 4
+                ["ы"] = 30,// 4
+                ["э"] = 31,// 5
+                ["ю"] = 32,// 1
+                ["я"] = 33 // 2
+            };
+
+
+            //Заполнение list
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(words[i].ToString()))
+                {
+                    list.Add(dict[words[i].ToString()]);
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка!");
+                }
+            }
+
+
+            //Кодирование
+            for (int i = 0; i < list.Count; i++)
+            {
+                //ONE
+                if (oneList.Contains(list[i]))
+                {
+                    for (int j = 0; j < qrCode.Length; j++)
+                    {
+                        for (int x = 0; x < qrCode[j].Length; x++)
+                        {
+                            if ((x == y1 || x == y2) && (j == x1 || j == x2 || j == x3))
+                            {
+                                if (qrCode[j][x].Equals(list[i])) // +
+                                {
+                                    qrCode[j][x] = "#";
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (twoList.Contains(list[i]))
+                {
+                    for (int n = 0; n < oneList.Count; n++)
+                    {
+                        for (int v = 0; v < oneList.Count; v++)
+                        {
+                            //TWO
+                            if (oneList[n] + oneList[v] == list[i])
+                            {
+                                for (int k = 0; k < qrCode.Length; k++)
+                                {
+                                    for (int l = 0; l < qrCode[k].Length; l++)
+                                    {
+                                        if ((l == y1 || l == y2) && (k == x1 || k == x2 || k == x3))
+                                        {
+                                            if (qrCode[k][l].Equals(oneList[n]) || qrCode[k][l].Equals(oneList[v]))
+                                            {
+                                                qrCode[k][l] = "#";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+                else if (threeList.Contains(list[i]))
+                {
+                    for (int p = 0; p < oneList.Count; p++)
+                    {
+                        for (int o = 0; o < oneList.Count; o++)
+                        {
+                            for (int h = 0; h < oneList.Count; h++)
+                            {
+                                //THREE
+                                if (oneList[p] + oneList[o] + oneList[h] == list[i])
+                                {
+                                    for (int k = 0; k < qrCode.Length; k++)
+                                    {
+                                        for (int l = 0; l < qrCode[k].Length; l++)
+                                        {
+                                            if ((l == y1 || l == y2) && (k == x1 || k == x2 || k == x3))
+                                            {
+                                                if (qrCode[k][l].Equals(oneList[p]) || qrCode[k][l].Equals(oneList[o]) || qrCode[k][l].Equals(oneList[h]))
+                                                {
+                                                    qrCode[k][l] = "#";
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (fourList.Contains(list[i]))
+                {
+                    for (int q = 0; q < oneList.Count; q++)
+                    {
+                        for (int w = 0; w < oneList.Count; w++)
+                        {
+                            for (int e = 0; e < oneList.Count; e++)
+                            {
+                                for (int m = 0; m < oneList.Count; m++)
+                                {
+                                    //FOUR
+                                    if (oneList[q] + oneList[w] + oneList[e] + oneList[m] == list[i])
+                                    {
+                                        for (int k = 0; k < qrCode.Length; k++)
+                                        {
+                                            for (int l = 0; l < qrCode[k].Length; l++)
+                                            {
+                                                if ((l == y1 || l == y2) && (k == x1 || k == x2 || k == x3))
+                                                {
+                                                    if (qrCode[k][l].Equals(oneList[q]) || qrCode[k][l].Equals(oneList[w]) || qrCode[k][l].Equals(oneList[e]) || qrCode[k][l].Equals(oneList[m]))
+                                                    {
+                                                        qrCode[k][l] = "#";
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int b = 0; b < oneList.Count; b++)
+                    {
+                        for (int t = 0; t < oneList.Count; t++)
+                        {
+                            for (int y = 0; y < oneList.Count; y++)
+                            {
+                                for (int u = 0; u < oneList.Count; u++)
+                                {
+                                    for (int g = 0; g < oneList.Count; g++)
+                                    {
+                                        //FIVE
+                                        if (oneList[b] + oneList[t] + oneList[y] + oneList[u] + oneList[g] == list[i])
+                                        {
+                                            for (int k = 0; k < qrCode.Length; k++)
+                                            {
+                                                for (int l = 0; l < qrCode[k].Length; l++)
+                                                {
+                                                    if ((l == y1 || l == y2) && (k == x1 || k == x2 || k == x3))
+                                                    {
+                                                        if (qrCode[k][l].Equals(oneList[b]) || qrCode[k][l].Equals(oneList[t]) || qrCode[k][l].Equals(oneList[y]) || qrCode[k][l].Equals(oneList[u]) || qrCode[k][l].Equals(oneList[g]))
+                                                        {
+                                                            qrCode[k][l] = "#";
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing;
+
+namespace QRCodeGenerate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[][] qrCode = new object[10][];
+
+            qrCode[0] = new object[] { "#", "#", "#", "#", "#", "#", " ", "#", " ", "#" };
+            qrCode[1] = new object[] { "#", " ", " ", " ", " ", "#", " ", 1, 2, 4 };
+            qrCode[2] = new object[] { "#", " ", "#", "#", " ", "#", " ", 8, 16, 32 };
+            qrCode[3] = new object[] { "#", " ", "#", "#", " ", "#", " ", 1, 2, 4 };
+            qrCode[4] = new object[] { "#", " ", " ", " ", " ", "#", " ", 8, 16, 32 };
+            qrCode[5] = new object[] { "#", "#", "#", "#", "#", "#", " ", 1, 2, 4 };
+            qrCode[6] = new object[] { " ", " ", " ", " ", " ", " ", " ", 8, 16, 32 };
+            qrCode[7] = new object[] { "#", 1, 2, 1, 2, 1, 2, 1, 2, 4 };
+            qrCode[8] = new object[] { " ", 4, 8, 4, 8, 4, 8, 8, 16, 32 };
+            qrCode[9] = new object[] { "#", 16, 32, 16, 32, 16, 32, "#", " ", "#" };
+
+
+            Console.Write("Какое слвово(только русский афавит) вы хотите закодировать(от 1 до 7): ");
+            string words = Console.ReadLine().ToLower();
+            if (words.Length < 1 || words.Length > 7)
+            {
+                Console.WriteLine("В не диапазона!");
+            }
+            else
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Coding.ShitCoding(qrCode, words[i].ToString(), 2, 1, 7, 8, 9);
+                    }
+                    if (i == 1)
+                    {
+                        Coding.ShitCoding(qrCode, words[i].ToString(), 4, 3, 7, 8, 9);
+                    }
+                    if (i == 2)
+                    {
+                        Coding.ShitCoding(qrCode, words[i].ToString(), 6, 5, 7, 8, 9);
+                    }
+                    if (i == 3)
+                    {
+                        Coding.ShitCoding(qrCode, words[i].ToString(), 8, 7, 7, 8, 9);
+                    }
+                    if (i == 4)
+                    {
+                        CodingHorizontal.EndWay(qrCode, words[i].ToString(), 6, 5, 7, 8, 9);
+                    }
+                    if (i == 5)
+                    {
+                        CodingHorizontal.EndWay(qrCode, words[i].ToString(), 4, 3, 7, 8, 9);
+                    }
+                    if (i == 6)
+                    {
+                        CodingHorizontal.EndWay(qrCode, words[i].ToString(), 2, 1, 7, 8, 9);
+                    }
+                }
+
+                //Удаление лишних чисел
+                double num;
+                for (int i = 0; i < qrCode.Length; i++)
+                {
+                    for (int j = 0; j < qrCode[i].Length; j++)
+                    {
+                        if (double.TryParse(qrCode[i][j].ToString(), out num))
+                        {
+                            qrCode[i][j] = " ";
+                        }
+                    }
+
+                }
+
+
+                //Вывод QR-Code в консоль
+                for (int i = 0; i < qrCode.Length; i++)
+                {
+                    for (int j = 0; j < qrCode[i].Length; j++)
+                    {
+                        Console.Write(qrCode[i][j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Photo(qrCode);
+
+            }
+
+        }
+
+        const int Pixel_Size = 20;
+        public static void Photo(object[][] qrCode)
+        {
+            Bitmap Result = new Bitmap(qrCode.Length * Pixel_Size, qrCode.Length * Pixel_Size);
+            Graphics Gr = Graphics.FromImage(Result);
+
+            for (int y = 0; y < qrCode.Length; y++)
+            {
+                for (int x = 0; x < qrCode[y].Length; x++)
+                {
+                    if (qrCode[y][x].ToString() == "#")
+                    {
+                        Gr.FillRectangle(Brushes.Black, new Rectangle(new Point(x * Pixel_Size, y * Pixel_Size), new Size(Pixel_Size, Pixel_Size)));
+                    }
+                    
+                }
+            }
+            Console.WriteLine("PNG Save");
+            Result.Save(@"C:\Users\00ARM-IT\Desktop\Hacker\QRCode.png", ImageFormat.Png);
+            
+        }
+
+    }
+}
